f58f0d427a675e6b584de927bcfe86580d3ac0c0
Ryan Cohen <ryjco20@gmail.com>
2021-10-10 01:09:38 -0400

    Changed String lengths for db schema

--- service/models.py
+++ service/models.py
@@ -27,10 +27,10 @@ class Customer(db.Model):
 
     # Table Schema
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(16), unique=True, nullable=False)
-    password = db.Column(db.String(16), nullable=False)
-    first_name = db.Column(db.String(35), nullable=False)
-    last_name = db.Column(db.String(35), nullable=False)
+    username = db.Column(db.String(64), unique=True, nullable=False)
+    password = db.Column(db.String(64), nullable=False)
+    first_name = db.Column(db.String(32), nullable=False)
+    last_name = db.Column(db.String(32), nullable=False)
     addresses = db.Column(db.ARRAY(db.String(100)), nullable=False)
 
     def __repr__(self):
