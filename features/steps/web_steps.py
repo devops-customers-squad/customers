2f078fdbd5c38e684142acb8a4a74b34d6db4eef
Bobby-zien <16zy10@queensu.ca>
2021-11-26 17:33:04 -0500

    added BDD testing for updating customer with missing values

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -76,6 +76,13 @@ def step_impl(context, element_name, text_string):
     element.clear()
     element.send_keys(text_string)
 
+@when('I set the "{element_name}" to ""')
+def step_impl(context, element_name):
+    element_id = mapping_name(element_name)
+    element = context.driver.find_element_by_id(element_id)
+    element.clear()
+    element.send_keys("")
+
 @when('I clear the "{element_name}" field')
 def step_impl(context, element_name):
     element_id = mapping_name(element_name)
