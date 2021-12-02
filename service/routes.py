ab691cf9e8a4b66129f6a49e247495a7c07514c7
Bobby-zien <16zy10@queensu.ca>
2021-12-01 23:41:08 -0500

    feat: swagger docs for /customer endpoints

--- service/routes.py
+++ service/routes.py
@@ -77,7 +77,10 @@ create_customer_model = api.inherit(
     'CustomerWithAddress', 
     update_customer_model,
     {
-        'addresses': fields.List(fields.Nested(address_model),
+        # Change address_model to create_address_model
+        # because we cannot provide a customer id or address id 
+        # when we are creating an new customer
+        'addresses': fields.List(fields.Nested(create_address_model),
                                 required=True,
                                 description='The addresses belonging to the customer')                       
     }    
@@ -468,6 +471,10 @@ class CustomerCollection(Resource):
     #------------------------------------------------------------------
     # CREATE A NEW CUSTOMER
     #------------------------------------------------------------------
+    @api.doc('create_customers')
+    @api.response(400, 'The posted data was not valid')
+    @api.expect(create_customer_model)
+    @api.marshal_with(customer_model, code=201)
     def post(self):
         """
         Creates a Customer
@@ -491,6 +498,9 @@ class CustomerCollection(Resource):
     #------------------------------------------------------------------
     # LIST ALL CUSTOMERS
     #------------------------------------------------------------------
+    @api.doc('list_customers')
+    @api.expect(customer_args, validate=True)
+    @api.marshal_list_with(customer_model)
     def get(self):
         """Returns all of the customers"""
         app.logger.info("Request for customer list")
@@ -500,10 +510,11 @@ class CustomerCollection(Resource):
             if key not in all_query_key:
                 raise UnsupportedKeyError("The query key: '" + key + "' is not supported.")
         
-        username = request.args.get("username")
-        first_name = request.args.get("first_name")
-        last_name = request.args.get("last_name")
-        prefix_username = request.args.get("prefix_username")
+        args = customer_args.parse_args()
+        username = args["username"]
+        first_name = args["first_name"]
+        last_name = args["last_name"]
+        prefix_username = args["prefix_username"]
 
         def filter(customers1, customers2):
             filter_customers = []
