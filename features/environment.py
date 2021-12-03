f46b8e1f3d0585b690a658ee1e8b8de077ff9b1d
Yongchang Liu <yl8192@nyu.edu>
2021-12-03 15:17:06 -0500

    fixed the DB URI for BDD tests

--- features/environment.py
+++ features/environment.py
@@ -2,11 +2,17 @@
 Environment for Behave Testing
 """
 from os import getenv
+import json
 from selenium import webdriver
 
 WAIT_SECONDS = int(getenv('WAIT_SECONDS', '60'))
 BASE_URL = getenv('BASE_URL', 'http://localhost:5000')
 
+# override if we are running in Cloud Foundry
+if 'VCAP_SERVICES' in os.environ:
+    vcap = json.loads(os.environ['VCAP_SERVICES'])
+    DATABASE_URI = vcap['user-provided'][0]['credentials']['url']
+    
 def before_all(context):
     """ Executed once before all tests """
     options = webdriver.ChromeOptions()
